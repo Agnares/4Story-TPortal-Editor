e9566aef0d521bab9d99a8a59c85ac57f9351ae5
Agnares <60239119+Agnares@users.noreply.github.com>
2023-01-09 14:53:24 +0100

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(202, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(333, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(547, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.addToolStripMenuItem,
+            this.removeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 329);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(192, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(126, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(192, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(126, 134);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(192, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(126, 160);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(192, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(126, 18);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(192, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(126, 44);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(192, 20);
+            this.textBox8.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(202, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(333, 204);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TPortal";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "m_wPortalID";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "m_fPosZ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "m_fPosX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "m_wMapID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "m_wPortalRegionID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "m_strNAME";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "byte - 0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "byte - 1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(202, 237);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(333, 78);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "lByteNotUsed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 366);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TPortal - Agnares";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<OpenFileDialog> lOfd = new List<OpenFileDialog>();
+        List<SaveFileDialog> lSfd = new List<SaveFileDialog>();
+
+        List<List<TPortalStruct>> loadedTcds = new List<List<TPortalStruct>>();
+
+        public class CBinaryReader : BinaryReader
+        {
+            public CBinaryReader(Stream input)
+                : base(input)
+            {
+            }
+
+            public CBinaryReader(Stream input, Encoding encoding) 
+                : base(input, encoding)
+            {
+            }
+
+            public CBinaryReader(Stream input, Encoding encoding, bool leaveOpen) 
+                : base(input, encoding, leaveOpen)
+            {
+            }
+
+            public string ReadCString()
+            {
+                ushort length = base.ReadByte();
+                if (length >= byte.MaxValue)
+                    length = base.ReadUInt16();
+                return Encoding.Default.GetString(base.ReadBytes(length));
+            }
+        }
+
+        public class CBinaryWriter : BinaryWriter
+        {
+            public CBinaryWriter(Stream input) 
+                : base(input)
+            {
+            }
+
+            public CBinaryWriter(Stream input, Encoding encoding) 
+                : base(input, encoding)
+            {
+            }
+
+            public CBinaryWriter(Stream input, Encoding encoding, bool leaveOpen) 
+                : base(input, encoding, leaveOpen)
+            {
+            }
+
+            public void WriteUInt16Length(int length)
+            {
+                base.Write(byte.MaxValue);
+                base.Write((ushort)length);
+            }
+
+            public void WriteByteLength(int length)
+            {
+                base.Write((byte)length);
+            }
+
+            public void WriteCString(string text)
+            {
+                int length = text.Length;
+                if (length >= byte.MaxValue)
+                    WriteUInt16Length(length);
+                else
+                    WriteByteLength(length);
+                base.Write(Encoding.Default.GetBytes(text));
+            }
+        }
+
+        public struct TPortalStruct
+        {
+            // Lists of Not Used
+            public List<byte> lByteNotUsed;
+
+            public ushort m_wPortalID { get; set; }
+            public string m_strNAME { get; set; }
+            public ushort m_wPortalRegionID { get; set; }
+            public ushort m_wMapID { get; set; }
+            public float m_fPosX { get; set; }
+            public float m_fPosZ { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Tcd files (TPortal.tcd)|TPortal.tcd";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+
+            lOfd.Clear();
+            loadedTcds.Clear();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                lOfd.Add(ofd);
+                ReadPortals();
+                FillListBox();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "All files (*.*)|*.*";
+            sfd.FilterIndex = 2;
+            sfd.RestoreDirectory = true;
+
+            lSfd.Clear();
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                lSfd.Add(sfd);
+                WritePortals();
+            }
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loadedTcds.Count > 0)
+            {
+                TPortalStruct sPortal = new TPortalStruct();
+
+                sPortal.lByteNotUsed = new List<byte>();
+                sPortal.lByteNotUsed.Add(new byte());
+                sPortal.lByteNotUsed.Add(new byte());
+
+                loadedTcds[0].Insert(listBox1.SelectedIndex + 1, sPortal);
+                FillListBox();
+            }
+        }
+
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (loadedTcds.Count > 0)
+            {
+                int nSelectedIndex = listBox1.SelectedIndex;
+                bool bIndex = nSelectedIndex >= 0 ? true : false;
+                if (bIndex)
+                    loadedTcds[0].RemoveAt(listBox1.SelectedIndex);
+                else
+                    MessageBox.Show("You have not selected any item to remove");
+
+                if ((nSelectedIndex - 1) < 0)
+                    FillListBox(0);
+                else
+                    FillListBox(nSelectedIndex - 1);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (loadedTcds.Count > 0)
+            {
+                TPortalStruct sPortal = loadedTcds[0][listBox1.SelectedIndex];
+                textBox1.Text = sPortal.m_wPortalID.ToString();
+                textBox2.Text = sPortal.m_strNAME;
+                textBox3.Text = sPortal.m_wPortalRegionID.ToString();
+                textBox4.Text = sPortal.m_wMapID.ToString();
+                textBox5.Text = sPortal.m_fPosX.ToString();
+                textBox6.Text = sPortal.m_fPosZ.ToString();
+                textBox7.Text = sPortal.lByteNotUsed[0].ToString();
+                textBox8.Text = sPortal.lByteNotUsed[1].ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (loadedTcds.Count > 0)
+            {
+                try
+                {
+                    TPortalStruct sPortal = new TPortalStruct();
+
+                    sPortal.lByteNotUsed = new List<byte>();
+                    sPortal.lByteNotUsed.Add(new byte());
+                    sPortal.lByteNotUsed.Add(new byte());
+
+                    sPortal.m_wPortalID = Convert.ToUInt16(textBox1.Text);
+                    sPortal.m_strNAME = textBox2.Text;
+                    sPortal.m_wPortalRegionID = Convert.ToUInt16(textBox3.Text);
+                    sPortal.m_wMapID = Convert.ToUInt16(textBox4.Text);
+                    sPortal.m_fPosX = Convert.ToSingle(textBox5.Text);
+                    sPortal.m_fPosZ = Convert.ToSingle(textBox6.Text);
+                    sPortal.lByteNotUsed[0] = Convert.ToByte(textBox7.Text);
+                    sPortal.lByteNotUsed[1] = Convert.ToByte(textBox8.Text);
+
+                    int nSelectedIndex = listBox1.SelectedIndex;
+                    loadedTcds[0][nSelectedIndex] = sPortal;
+                    FillListBox(nSelectedIndex);
+
+                    MessageBox.Show("Selected portal saved!");
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.ToString());
+                    MessageBox.Show("You have not selected any item to save");
+                }
+            }
+        }
+
+        private void ReadPortals()
+        {
+            for (int i = 0; i < lOfd.Count; i++)
+            {
+                ReadPortal(lOfd[i].OpenFile());
+            }
+        }
+
+        private void WritePortals()
+        {
+            for (int i = 0; i < lSfd.Count; i++)
+            {
+                WritePortal(lSfd[i].OpenFile());
+            }
+        }
+
+        private void FillListBox(int nSelectedIndex = -1)
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < loadedTcds[0].Count; i++)
+            {
+                string strPortal = i + ": " + loadedTcds[0][i].m_strNAME;
+                listBox1.Items.Add(strPortal);
+            }
+            if (nSelectedIndex != -1)
+                listBox1.SelectedIndex = nSelectedIndex;
+        }
+
+        private void ReadPortal(Stream stream)
+        {
+            CBinaryReader br = new CBinaryReader(stream);
+            List<TPortalStruct> loadedTcd = new List<TPortalStruct>();
+
+            ushort wCount = br.ReadUInt16();
+            for (ushort i = 0; i < wCount; i++)
+            {
+                TPortalStruct sPortal = new TPortalStruct();
+
+                // Lists of Not Used
+                sPortal.lByteNotUsed = new List<byte>();
+
+                sPortal.m_wPortalID = br.ReadUInt16();
+                sPortal.m_strNAME = br.ReadCString();
+                sPortal.m_wPortalRegionID = br.ReadUInt16();
+                sPortal.m_wMapID = br.ReadUInt16();
+                sPortal.m_fPosX = br.ReadSingle();
+                sPortal.m_fPosZ = br.ReadSingle();
+
+                sPortal.lByteNotUsed.Add(br.ReadByte());
+                sPortal.lByteNotUsed.Add(br.ReadByte());
+
+                loadedTcd.Add(sPortal);
+            }
+
+            loadedTcds.Add(loadedTcd);
+
+            br.Close();
+        }
+
+        private void WritePortal(Stream stream)
+        {
+            CBinaryWriter bw = new CBinaryWriter(stream);
+
+            if (loadedTcds.Count > 0)
+            {
+                List<TPortalStruct> lPortal = new List<TPortalStruct>(loadedTcds[0]);
+
+                ushort nTotalPortals = (ushort)lPortal.Count;
+
+                bw.Write(nTotalPortals);
+
+                for (ushort i = 0; i < nTotalPortals; i++)
+                {
+                    bw.Write(lPortal[i].m_wPortalID);
+                    bw.WriteCString(lPortal[i].m_strNAME);
+                    bw.Write(lPortal[i].m_wPortalRegionID);
+                    bw.Write(lPortal[i].m_wMapID);
+                    bw.Write(lPortal[i].m_fPosX);
+                    bw.Write(lPortal[i].m_fPosZ);
+                    bw.Write(lPortal[i].lByteNotUsed[0]);
+                    bw.Write(lPortal[i].lByteNotUsed[1]);
+                }
+            }
+
+            bw.Close();
+            MessageBox.Show("TPortal saved!");
+        }
+    }
+}
